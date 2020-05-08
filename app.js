d85c5f2e0ea565c9a4c245e82db478d075b46f4e
Simon <simonanewton@gmail.com>
2020-05-07 22:14:29 -0400

    2.0 code refactoring, fixed writeToOutput function

--- app.js
+++ app.js
@@ -114,25 +114,24 @@ async function promptUser() {
     return employees;
 }
 
-//----------------------------------------------------------------------------------------
-
 // writes html to the output folder in the root, if no output folder exists, creates one
 function writeToOutput(data) {
     fs.access(OUTPUT_DIR, (err) => {
         if (err) {
             fs.mkdir(OUTPUT_DIR, (err) => {
                 if (err) throw err;
-            })
+            });
         }
+        fs.writeFile(outputPath, data, (err) => {
+            if (err) throw err;
+    
+            console.log("Success!");
+            console.log("------");
+        });
     });
-
-    fs.writeFile(outputPath, data, (err) => {
-        if (err) throw err;
-
-        console.log("Success!");
-        console.log("------");
-    })
 }
+
+//----------------------------------------------------------------------------------------
 
 // prompts the user for employee information, renders HTML with that information, writes the HTML to a file in the output folder
 async function init() {
@@ -144,34 +143,3 @@ async function init() {
 }
 
 init();
-
-//----------------------------------------------------------------------------------------
-
-// TO-DO:
-// - fix render output for multiple employees
-// - fix check for existing output folder
-
-//----------------------------------------------------------------------------------------
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
